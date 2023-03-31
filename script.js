e7de197d559c83681bfeebd1e0fef25b6331602b
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-03-31 21:04:36 +0600

    formated JavaScript document

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const newGameContainer = document.getElementById("new-game-container");
 const newGameButton = document.getElementById("new-game-button");
 const canvas = document.getElementById("canvas");
 const resultText = document.getElementById("result-text");
-
 //Options values for buttons
 let options = {
   fruits: [
@@ -27,13 +26,10 @@ let options = {
     "Dominica",
   ],
 };
-
 //count
 let winCount = 0;
 let count = 0;
-
 let chosenWord = "";
-
 //Display option buttons
 const displayOptions = () => {
   optionsContainer.innerHTML += `<h3>Please Select An Option</h3>`;
@@ -43,7 +39,6 @@ const displayOptions = () => {
   }
   optionsContainer.appendChild(buttonCon);
 };
-
 //Block all the Buttons
 const blocker = () => {
   let optionsButtons = document.querySelectorAll(".options");
@@ -52,14 +47,12 @@ const blocker = () => {
   optionsButtons.forEach((button) => {
     button.disabled = true;
   });
-
   //disable all letters
   letterButtons.forEach((button) => {
     button.disabled.true;
   });
   newGameContainer.classList.remove("hide");
 };
-
 //Word Generator
 const generateWord = (optionValue) => {
   let optionsButtons = document.querySelectorAll(".options");
@@ -70,35 +63,28 @@ const generateWord = (optionValue) => {
     }
     button.disabled = true;
   });
-
   //initially hide letters, clear previous word
   letterContainer.classList.remove("hide");
   userInputSection.innerText = "";
-
   let optionArray = options[optionValue];
   //choose random word
   chosenWord = optionArray[Math.floor(Math.random() * optionArray.length)];
   chosenWord = chosenWord.toUpperCase();
-
   //replace every letter with span containing dash
   let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
-
   //Display each element as span
   userInputSection.innerHTML = displayItem;
 };
-
 //Initial Function (Called when page loads/user presses new game)
 const initializer = () => {
   winCount = 0;
   count = 0;
-
   //Initially erase all content and hide letteres and new game button
   userInputSection.innerHTML = "";
   optionsContainer.innerHTML = "";
   letterContainer.classList.add("hide");
   newGameContainer.classList.add("hide");
   letterContainer.innerHTML = "";
-
   //For creating letter buttons
   for (let i = 65; i < 91; i++) {
     let button = document.createElement("button");
@@ -142,54 +128,44 @@ const initializer = () => {
     });
     letterContainer.append(button);
   }
-
   displayOptions();
   //Call to canvasCreator (for clearing previous canvas and creating initial canvas)
   let { initialDrawing } = canvasCreator();
   //initialDrawing would draw the frame
   initialDrawing();
 };
-
 //Canvas
 const canvasCreator = () => {
   let context = canvas.getContext("2d");
   context.beginPath();
   context.strokeStyle = "#000";
   context.lineWidth = 2;
-
   //For drawing lines
   const drawLine = (fromX, fromY, toX, toY) => {
     context.moveTo(fromX, fromY);
     context.lineTo(toX, toY);
     context.stroke();
   };
-
   const head = () => {
     context.beginPath();
     context.arc(70, 30, 10, 0, Math.PI * 2, true);
     context.stroke();
   };
-
   const body = () => {
     drawLine(70, 40, 70, 80);
   };
-
   const leftArm = () => {
     drawLine(70, 50, 50, 70);
   };
-
   const rightArm = () => {
     drawLine(70, 50, 90, 70);
   };
-
   const leftLeg = () => {
     drawLine(70, 80, 50, 110);
   };
-
   const rightLeg = () => {
     drawLine(70, 80, 90, 110);
   };
-
   //initial frame
   const initialDrawing = () => {
     //clear canvas
@@ -203,10 +179,8 @@ const canvasCreator = () => {
     //small top line
     drawLine(70, 10, 70, 20);
   };
-
   return { initialDrawing, head, body, leftArm, rightArm, leftLeg, rightLeg };
 };
-
 //draw the man
 const drawMan = (count) => {
   let { head, body, leftArm, rightArm, leftLeg, rightLeg } = canvasCreator();
@@ -233,7 +207,6 @@ const drawMan = (count) => {
       break;
   }
 };
-
 //New Game
 newGameButton.addEventListener("click", initializer);
 window.onload = initializer;
